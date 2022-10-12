98e375368aed9b4bddc0cb157db0149d668f246e
gdietz8 <83431340+gdietz8@users.noreply.github.com>
2022-10-12 13:33:52 -0700

    fixing the reverse methods

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,11 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+  
+  @Test
+  public void testReversed2() {
+    int[] input1 = {4,5};
+    assertArrayEquals(new int[]{5,4}, ArrayExamples.reversed(input1));
+  }
+
 }
